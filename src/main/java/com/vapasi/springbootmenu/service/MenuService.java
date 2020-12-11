ad74fa96f834ae2d293c5578a8eb8ac3a91e982a
Sakthe Karthika <sakthe.karthika@gmail.com>
2020-12-11 09:44:48 +0530

    [Sakthe] Add code for Integration on Spring boot

--- src/main/java/com/vapasi/springbootmenu/service/MenuService.java
+++ src/main/java/com/vapasi/springbootmenu/service/MenuService.java
@@ -6,6 +6,7 @@ import com.vapasi.springbootmenu.request.MenuDto;
 import com.vapasi.springbootmenu.response.MenuResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +36,7 @@ public class MenuService {
         return listMenuResponse;
     }
 
+    @Transactional
     public void deleteMenu(String name) {
         menuRepository.deleteByName(name);
     }
--- src/test/java/com/vapasi/springbootmenu/controller/MenuControllerITTest.java
+++ src/test/java/com/vapasi/springbootmenu/controller/MenuControllerITTest.java
@@ -1,5 +1,7 @@
 package com.vapasi.springbootmenu.controller;
 
+import com.vapasi.springbootmenu.entity.Menu;
+import com.vapasi.springbootmenu.request.MenuDto;
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +9,10 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
@@ -22,15 +27,19 @@ public class MenuControllerITTest {
         JSONAssert.assertEquals("[{name:Dosa} , {name:Idly} ,{name:Vada}]" , response , false);
     }
 
-    /*@Test
+    @Test
     public void shouldSaveTheMenu() throws JSONException {
-        String response = this.restTemplate.postForObject("/menu" , String.class);
-        JSONAssert.assertEquals("[{name:Dosa} , {name:Idly} ,{name:Vada}]" , response , false);
-    }*/
+        MenuDto menuDto = new MenuDto("Noodles" , 200);
+        ResponseEntity<String> responseEntity = this.restTemplate.postForEntity("/menu" , menuDto , String.class);
+        assertEquals(200, responseEntity.getStatusCodeValue());
+    }
 
     @Test
     public void shouldDeleteTheMenu() throws JSONException {
         this.restTemplate.delete("/menu/Dosa");
+        String response = this.restTemplate.getForObject("/menu" , String.class);
+        System.out.println(response);
+        JSONAssert.assertEquals("[{name:Idly} ,{name:Vada}]" , response , false);
 
     }
 }
